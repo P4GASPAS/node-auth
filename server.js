7ab3651b339e22400ae9146ca98f0704c17a29fa
Yhestin <yhestin10@gmail.com>
2024-09-11 01:43:33 +0800

    initial config for express server and nodemon as a dev env

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log('hello world')
+const express = require('express')
+
+const server = express()
+
+server.get('/', (req, res) => res.send('Welcome to auth service'))
+
+const PORT = 2000
+server.listen(PORT, () => {
+    console.log(`Server is running at port ${PORT}`)
+})
